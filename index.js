0ca2d8733e5cd05e73c7fd88950fb5e38bdf9f10
ktinson <kaitinse@gmail.com>
2024-12-01 13:15:43 -0600

    Added Auth0 router

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config('.env');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { PORT = 3000 } = process.env;
+const { auth } = require('express-openid-connect');
+// TODO - require express-openid-connect and destructure auth from it
+
+const { User, Cupcake } = require('./db');
+
+// middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+/* *********** YOUR CODE HERE *********** */
+// follow the module instructions: destructure config environment variables from process.env
+// follow the docs:
+  // define the config object
+  // attach Auth0 OIDC auth router
+  // create a GET / route handler that sends back Logged in or Logged out
+
+  const {
+    AUTH0_SECRET,
+    AUTH0_AUDIENCE,
+    AUTH0_CLIENT_ID,
+    AUTH0_BASE_URL,
+  } = process.env;
+  
+  // Auth0 router config
+  const config = {
+    authRequired: true,
+    auth0Logout: true,
+    secret: AUTH0_SECRET,
+    baseURL: AUTH0_AUDIENCE,
+    clientID: AUTH0_CLIENT_ID,
+    issuerBaseURL: AUTH0_BASE_URL,
+  };
+  
+  app.use(auth(config)); // Attach the Auth0 router
+  
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+app.get('/cupcakes', async (req, res, next) => {
+  try {
+    const cupcakes = await Cupcake.findAll();
+    res.send(cupcakes);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// error handling middleware
+app.use((error, req, res, next) => {
+  console.error('SERVER ERROR: ', error);
+  if(res.statusCode < 400) res.status(500);
+  res.send({error: error.message, name: error.name, message: error.message});
+});
+
+app.listen(PORT, () => {
+  console.log(`Cupcakes are ready at http://localhost:${PORT}`);
+});
+
